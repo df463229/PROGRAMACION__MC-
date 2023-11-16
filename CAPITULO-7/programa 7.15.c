b29572a7843a429c4d144dee9163752081c741d1
df463229 <144465609+df463229@users.noreply.github.com>
2023-11-16 18:14:49 -0400

    CAPITULO 7

---
+++ CAPITULO-7/programa 7.15.c
@@ -0,0 +1,33 @@
+# include <stdio.h>
+# include <ctype.h>
+/* Decodifica.
+El programa decodifica una cadena de caracteres compuesta por números y
+letras. */
+
+void interpreta(char *); /* Prototipo de función. */
+
+void main(void)
+{
+char cadena[50];
+printf("\nIngrese la cadena de caracteres: ");
+gets(cadena);
+interpreta(cadena);
+}
+void interpreta(char *cadena)
+/* Esta función se utiliza para decodificar la cadena de caracteres. */
+{
+int i = 0, j, k;
+while (cadena[i] != '\0')
+{
+if (isalpha (cadena[i])) /* Se utiliza isalpha para observar si el caracteres una letra. */
+{
+k = cadena[i - 1] - 48;
+/* En la variable entera k se almacena el ascii del número —convertido
+en caracter— que nos interesa, menos 48 que corresponde al ascii
+del dígito 0. */
+for (j = 0; j < k; j++)
+putchar(cadena[i]);
+}
+i++;
+}
+}
